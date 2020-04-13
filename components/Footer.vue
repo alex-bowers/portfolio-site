662e8d5be81047c256eecbdb11e4b448f55bae9e
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2020-04-13 16:42:19 +0100

    Add footer component

---
+++ components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="footer">
+        <social-links></social-links>
+    </div>
+</template>
+
+<script>
+import SocialLinks from './Common/Social.vue'
+
+export default {
+    components: {
+        SocialLinks
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/assets/scss/colours.scss';
+
+.footer {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: sticky;
+    bottom: 0;
+    background-color: $primary-colour;
+    padding: 2rem;
+    z-index: -1;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,20 @@
         <hero></hero>
         <about-me></about-me>
         <happy-dev></happy-dev>
+        <site-footer></site-footer>
     </div>
 </template>
 
 <script>
 import AboutMe from '~/components/AboutMe.vue'
+import SiteFooter from '~/components/Footer.vue'
 import HappyDev from '~/components/HappyDev.vue'
 import Hero from '~/components/Hero.vue'
 
 export default {
     components: {
         AboutMe,
+        SiteFooter,
         HappyDev,
         Hero
     }
