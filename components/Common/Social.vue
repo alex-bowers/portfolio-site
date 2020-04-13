53af370e1cd236b6082c4ec42f765b10b1543468
Alex Bowers <alex.bowers@bluebaytravel.co.uk>
2020-04-13 13:44:02 +0100

    Add a common social media component

---
+++ components/Common/Social.vue
@@ -0,0 +1,63 @@
+<template>
+    <div>
+        <a
+            :class="{ 'no-end-padding': $props.hasNoPaddingOnEnds }"
+            :href="profiles.linkedin"
+            :style="{ padding: $props.padding }"
+            target="_blank"
+        >
+            <img src="../../assets/images/linkedin.svg" alt="LinkedIn Link" />
+        </a>
+        <a
+            :class="{ 'no-end-padding': $props.hasNoPaddingOnEnds }"
+            :href="profiles.github"
+            :style="{ padding: $props.padding }"
+            target="_blank"
+        >
+            <img src="../../assets/images/github.svg" alt="LinkedIn Link" />
+        </a>
+        <a
+            :class="{ 'no-end-padding': $props.hasNoPaddingOnEnds }"
+            :href="profiles.twitter"
+            :style="{ padding: $props.padding }"
+            target="_blank"
+        >
+            <img src="../../assets/images/twitter.svg" alt="LinkedIn Link" />
+        </a>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        hasNoPaddingOnEnds: {
+            default: false,
+            type: Boolean
+        },
+        padding: {
+            default: '5px',
+            type: String
+        }
+    },
+    data() {
+        return {
+            profiles: {
+                github: 'https://github.com/alex-bowers',
+                linkedin: 'https://www.linkedin.com/in/alex-bowers-15874354/',
+                twitter: 'https://twitter.com/alex_bowers'
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.no-end-padding {
+    &:first-child {
+        padding-left: 0px !important;
+    }
+    &:last-child {
+        padding-right: 0px !important;
+    }
+}
+</style>
